6d9635b711ccec50e66bda7f9818a6f7d691622e
Julian Olbinski <163324229+jolbinsk1@users.noreply.github.com>
2024-07-13 09:06:58 +0200

    Create english_return_function.py

---
+++ english_return_function.py
@@ -0,0 +1,46 @@
+# function for returning the English words for numbers 0 to 1,000,000
+
+def return_words(num):
+    
+    if num < 10:
+        return aughts[num]
+    
+    elif num < 20:
+        return ten_thru_teens[num]
+    
+    elif num < 100:
+        if num%10==0:
+            return tens[num]
+        else:
+            return tens[(num//10)*10] + '-' + aughts[num%10]
+        
+    elif num < 1000:
+        if num%100==0:
+            return aughts[num//100] + ' hundred'
+    
+        else:
+            return aughts[num//100] + ' hundred ' + return_words(num%100)
+    
+    elif num < 10000:
+        if num%1000==0:
+            return aughts[num//1000] + ' thousand'
+            
+        else:
+            return aughts[num//1000] + ' thousand ' + return_words(num%1000)
+            
+    elif num < 100000:
+        if num%1000==0:
+            return return_words(num//1000) + ' thousand'
+        
+        else:
+            return return_words(num//1000) + ' thousand ' + return_words(num%1000)
+        
+    elif num < 1000000:
+        if num%1000==0:
+            return return_words(num//1000) + ' thousand'
+        else:
+            return return_words(num//1000) + ' thousand ' + return_words(num%1000)
+
+    elif num == 1000000:
+        
+        return "one million"
