194a100bf0815e5b997bff296b7e926742e5f96e
Julian Olbinski <163324229+jolbinsk1@users.noreply.github.com>
2024-07-13 09:02:37 +0200

    Create english_polish_dictionaries.py

---
+++ english_polish_dictionaries.py
@@ -0,0 +1,57 @@
+# first we will make our dictionaries
+
+
+# English dictionary
+
+aughts = {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+
+ten_thru_teens = {10:'ten',11:'eleven',12:'twelve',13:'thirteen',14:'fourteen',15:'fifteen',16:'sixteen',
+       17:'seventeen',18:'eighteen',19:'nineteen'}
+
+tens = {20:'twenty',30:'thirty',40:'forty',50:'fifty',60:'sixty',70:'seventy',80:'eighty',90:'ninety'}
+
+
+
+# Polish dictionary
+
+aughts_PL = {0: 'zero', 1: 'jeden', 2:'dwa', 3:'trzy', 4:'cztery',
+    5:'pięć', 6:'sześć', 7:'siedem', 8:'osiem', 9:'dziewięć'}
+
+ten_thru_teens_PL = {10: 'dziesięć', 11: 'jedenaście', 12: 'dwanaście', 13: 'trzynaście', 14: 'czternaście', 
+    15: 'piętnaście', 16: 'szesnaście', 17: 'siedemnaście',18: 'osiemnaście',19: 'dziewiętnaście'}
+
+tens_PL = {20: 'dwadzieścia',30: 'trzydzieści',40: 'czterdzieści',50: 'pięćdziesiąt',
+    60: 'sześćdziesiąt',70: 'siedemdziesiąt',80: 'osiemdziesiąt',90: 'dziewięćdziesiąt'}
+
+hundreds_PL = {100: 'sto', 200: 'dwieście',300: 'trzysta',400: 'czterysta',
+    500: 'pięćset',600: 'sześćset',700: 'siedemset',800: 'osiemset',900: 'dziewięćset'}
+
+
+# Polish IPA dictionary
+
+aughts_PL_IPA = {0: "ˈzɛ.rɔ", 1: "ˈjɛ.dɛn", 2: "dva", 3: "tʂɨ", 4: "ˈtʂtɛ.rɨ",5: "pjɛɲtɕ", 
+    6: "ʂɛɕtɕ", 7: "ˈɕɛ.dɛm", 8: "ˈɔ.ɕɛm", 9: "ˈd͡ʑɛ.vjɛɲtɕ"}
+
+ten_thru_teens_PL_IPA = {10: "ˈd͡ʑɛ.ɕɛɲtɕ", 11: "jɛ.dɛˈnaɕ.tɕɛ", 12: "dvaˈnaɕ.tɕɛ", 13: "tʂɨˈnaɕ.tɕɛ", 14: "tʂtɛrˈnaɕ.tɕɛ", 15: "pjɛntˈnaɕ.tɕɛ", 
+    16: "ʂɛsˈnaɕ.tɕɛ", 17: "ɕɛ.dɛmˈnaɕ.tɕɛ", 18: "ɔɕɛmˈnaɕ.tɕɛ", 19: "d͡ʑɛ.vjɛntˈnaɕ.tɕɛ"}
+
+tens_PL_IPA = {20: "dvaˈd͡ʑɛɕ.tɕa", 30: "tʂɨˈd͡ʑɛɕ.tɕi", 40: "tʂtɛrˈd͡ʑɛɕ.tɕi", 50: "pjɛɲd͡ʑɛˈɕɔnt", 
+    60: "ʂɛɕd͡ʑɛˈɕɔnt", 70: "ɕɛ.dɛm.d͡ʑɛˈɕɔnt", 80: "ɔ.ɕɛm.d͡ʑɛˈɕɔnt", 90: "d͡ʑɛ.vjɛɲ.d͡ʑɛˈɕɔnt"}
+
+hundreds_PL_IPA = {100: "stɔ", 200: "ˈdvjɛɕ.tɕɛ", 300: "ˈtʂɨ.sta", 400: "tʂtɛˈrɨ.sta", 500: "ˈpjɛɲsɛt", 
+    600: "ˈʂɛɕsɛt", 700: "ˈɕɛ.dɛm.sɛt", 800: "ˈɔ.ɕɛm.sɛt", 900: "ˈd͡ʑɛ.vjɛɲ.sɛt"}
+
+
+# Polish dictionary with English pronunciations
+
+aughts_PL_ENG_pro = {0: "ZEH-roh", 1: "YEH-den", 2: "dvah", 3: "tshih", 4: "TCH-teh-rih",5: "pyehnch", 
+    6: "shehshtch", 7: "SHYEH-dem", 8: "OH-shyem", 9: "JYEH-vyench"}
+
+ten_thru_teens_PL_ENG_pro = {10: "JYEH-shench", 11: "yeh-deh-NAH-shcheh", 12: "dvah-NAH-shcheh", 13: "tshih-NAH-shcheh", 14: "tch-tehr-NAH-shcheh", 15: "pyent-NAH-shcheh", 
+    16: "shes-NAH-shcheh", 17: "SHYEH-dem-NAH-shcheh", 18: "OH-shyem-NAH-shcheh", 19: "JYEH-vyent-NAH-shcheh"}
+
+tens_PL_ENG_pro = {20: "dvah-DZYEH-shchah", 30: "tshih-DZYEH-shchih", 40: "tch-tehr-DZYEH-shchih", 50: "pyench-DZYEH-shohnt", 60: "shehshtch-DZYEH-shohnt", 
+    70: "SHYEH-dem-DZYEH-shohnt", 80: "OH-shyem-DZYEH-shohnt", 90: "JYEH-vyen-DZYEH-shohnt"}
+
+hundreds_PL_ENG_pro = {100: "stoh", 200: "DVYEH-shcheh", 300: "TSH-stah", 400: "tch-TEH-rih-stah", 500: "pyent-set", 
+    600: "shehsht-set", 700: "SHYEH-dem-set", 800: "OH-shyem-set", 900: "JYEH-vyen-set"}
